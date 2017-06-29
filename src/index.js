d401d17388f09f8dd8232eb2d75002f3970a42a2
Simon Bloom <swbloom@gmail.com>
2017-06-29 14:04:01 -0400

    updated test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import { ConnectedRouter, routerMiddleware, push } from 'react-router-redux';
 import { Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
+import { isLoggedIn } from './services/authService';
 
 import rootReducer from './reducers/index';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
@@ -22,6 +23,12 @@ const store = createStore(
         middleware,
     ),
 );
+
+if (isLoggedIn()) {
+    store.dispatch(push('/dashboard'));
+} else {
+    store.dispatch(push('/login'));
+}
 
 const App = () => (
     <Provider store={store}>
