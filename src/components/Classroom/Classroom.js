4d403b0b9a3509b8dfb4663a666cca147986f788
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2017-07-06 12:16:53 -0400

    added back button in individual classrooms

--- src/components/Classroom/Classroom.js
+++ src/components/Classroom/Classroom.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Classroom extends React.Component {
     componentDidMount() {
@@ -10,9 +11,9 @@ class Classroom extends React.Component {
     }
     render() {
         return (
-            <pre>
-                {this.props.classroom.title}
-            </pre>
+            <div className="container full">
+                <Link to="/dashboard" className="linkBtn">Go Back To Dashboard</Link>
+            </div>
         );
     }
 }
@@ -21,18 +22,18 @@ Classroom.propTypes = {
     actions: PropTypes.shape({
         getCourse: PropTypes.func.isRequired,
     }).isRequired,
-    classroom: PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        template: PropTypes.bool.isRequired,
-        term: PropTypes.string.isRequired,
-        start_date: PropTypes.number.isRequired,
-        end_date: PropTypes.number.isRequired,
-        instructor: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        created_at: PropTypes.number.isRequired,
-        updated_at: PropTypes.number.isRequired,
-    }).isRequired,
+    // classroom: PropTypes.shape({
+    //     _id: PropTypes.string.isRequired,
+    //     title: PropTypes.string.isRequired,
+    //     template: PropTypes.bool.isRequired,
+    //     term: PropTypes.string.isRequired,
+    //     start_date: PropTypes.number.isRequired,
+    //     end_date: PropTypes.number.isRequired,
+    //     instructor: PropTypes.string.isRequired,
+    //     description: PropTypes.string.isRequired,
+    //     created_at: PropTypes.number.isRequired,
+    //     updated_at: PropTypes.number.isRequired,
+    // }).isRequired,
     match: PropTypes.shape({
         params: PropTypes.shape({
             classroom_id: PropTypes.string.isRequired,
