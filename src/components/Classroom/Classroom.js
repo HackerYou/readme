0374904815e58301ad043815ccaeec90b6e14361
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2017-07-06 14:41:11 -0400

    added LessonGroup component to the individual classrooms

--- src/components/Classroom/Classroom.js
+++ src/components/Classroom/Classroom.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import LessonGroup from '../LessonGroup/LessonGroup';
 
 class Classroom extends React.Component {
     componentDidMount() {
@@ -10,9 +11,25 @@ class Classroom extends React.Component {
         getCourse(classroom_id);
     }
     render() {
+        const { classroom } = this.props;
         return (
             <div className="container full">
                 <Link to="/dashboard" className="linkBtn">Go Back To Dashboard</Link>
+                <header>
+                    <h1>{classroom.title}</h1>
+                </header>
+                <section className="lessonsWrap clearfix">
+                    <ol className="lessonColumn">
+                        {classroom.sections.map((section) => {
+                            return (
+                                <LessonGroup
+                                    key={section._id}
+                                    group={section}
+                                />
+                            );
+                        })}
+                    </ol>
+                </section>
             </div>
         );
     }
@@ -22,9 +39,10 @@ Classroom.propTypes = {
     actions: PropTypes.shape({
         getCourse: PropTypes.func.isRequired,
     }).isRequired,
-    // classroom: PropTypes.shape({
+    classroom: PropTypes.shape({
     //     _id: PropTypes.string.isRequired,
-    //     title: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        sections: PropTypes.arrayOf(PropTypes.object).isRequired,
     //     template: PropTypes.bool.isRequired,
     //     term: PropTypes.string.isRequired,
     //     start_date: PropTypes.number.isRequired,
@@ -33,7 +51,7 @@ Classroom.propTypes = {
     //     description: PropTypes.string.isRequired,
     //     created_at: PropTypes.number.isRequired,
     //     updated_at: PropTypes.number.isRequired,
-    // }).isRequired,
+    }).isRequired,
     match: PropTypes.shape({
         params: PropTypes.shape({
             classroom_id: PropTypes.string.isRequired,
