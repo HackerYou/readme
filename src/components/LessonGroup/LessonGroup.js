23fef5f5675e301533509472fae1c13c5956fa24
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2017-07-06 16:58:56 -0400

    added lesson group section divs in dashboard

--- src/components/LessonGroup/LessonGroup.js
+++ src/components/LessonGroup/LessonGroup.js
@@ -1,11 +1,22 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
-const LessonGroup = () => {
+const LessonGroup = (props) => {
+    const lesson = props;
+    console.log('hello', lesson.group);
     return (
-        <pre>
-            Lesson Group
-        </pre>
+        <li className="lessonGroup">
+            <h3>{lesson.group.title}</h3>
+            <div className="card lessonCard">
+                <ol className="lessonSection">
+                    {lesson.group.lessons.map((item) => {
+                        return (
+                            <li className="lessonRow">{item.title}</li>
+                        );
+                    })}
+                </ol>
+            </div>
+        </li>
     );
 };
 
