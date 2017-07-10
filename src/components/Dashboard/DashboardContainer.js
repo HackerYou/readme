5c8abfc4d0c7b2aed040afa6315102edb24e7c5c
Simon Bloom <swbloom@gmail.com>
2017-07-10 15:56:20 -0400

    Adds fontawesome, removes example component and adds in the admin panel component

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -4,8 +4,8 @@ import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import Dashboard from './Dashboard';
 
-const mapStateToProps = ({ auth, course }) => {
-    return { auth, course };
+const mapStateToProps = ({ auth, course, user }) => {
+    return { auth, course, user };
 };
 
 const mapDispatchToProps = dispatch => ({
