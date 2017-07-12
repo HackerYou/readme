a55aea58f50c0e042e118205b088bc3351e27721
Simon Bloom <swbloom@gmail.com>
2017-07-12 12:04:40 -0400

    fixing tests

--- src/actions/loaderActions/loaderActions.test.js
+++ src/actions/loaderActions/loaderActions.test.js
@@ -2,7 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import * as actions from './loaderActions';
 import expect from 'expect';
-import types from './actionTypes';
+import types from '../actionTypes';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
