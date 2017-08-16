2df36882fe7ca1b76c99e826eb341f2003697460
Simon Bloom <swbloom@gmail.com>
2017-08-16 10:54:43 -0400

    Adds validation and functionality for creating course templates

--- src/components/CourseTemplates/CourseTemplates.js
+++ src/components/CourseTemplates/CourseTemplates.js
@@ -1,24 +1,91 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import Input from '../Forms/Input';
+import PropTypes from 'prop-types';
 
-const CourseTemplates = () => {
-    return (
-        <div className="container">
-            <header className="topContent">
-                <Link to="/dashboard" className="linkBtn">
-                    <button className="primary">
-                        <i className="chalk-home" />back to dashboard</button>
-                </Link>
-                <h1>Course Templates</h1>
-            </header>
-            <section className="full card detailsForm">
-                <form action="">
-                    <div className="fieldRow" />
-                </form>
+import Input from '../Forms/Input/Input';
+import { run, ruleRunner } from '../../utils/forms/ruleRunner';
+import { required } from '../../utils/forms/rules';
+
+const fieldValidations = [
+    ruleRunner('title', 'Template title', required),
+];
+
+class CourseTemplates extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: '',
+            validationErrors: {},
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.createTemplate = this.createTemplate.bind(this);
+    }
+    componentDidMount() {
+        this.setState({ validationErrors: run(this.state, fieldValidations) });
+    }
+    handleInput(e) {
+        const newState = Object.assign({}, this.state, {
+            [e.target.name]: e.target.value,
+        });
+        newState.validationErrors = run(newState, fieldValidations);
+        this.setState(newState);
+    }
+    createTemplate(e) {
+        e.preventDefault();
+        const { broadcast } = this.props.actions;
+        const { validationErrors, title } = this.state;
+        const prefix = 'Please resolve the following errors: ';
+        const errorKeys = Object.keys(validationErrors);
+
+        if (errorKeys.length > 0) {
+            let errors = errorKeys.map(error => validationErrors[error]);
+            errors = errors.join(', ');
+            broadcast(prefix + errors, 'error');
+            return null;
+        }
+        return this.props.actions.createTemplateThunk({
+            title,
+        });
+    }
+    render() {
+        return (
+            <section className="mainContent">
+                <div className="container">
+                    <header className="topContent">
+                        <Link to="/dashboard" className="linkBtn">
+                            <button className="primary">
+                                <i className="chalk-home" />back to dashboard</button>
+                        </Link>
+                        <h1>Course Templates</h1>
+                    </header>
+                </div>
+                <section className="full card detailsForm">
+                    <form action="" onSubmit={this.createTemplate}>
+                        <div className="fieldRow">
+                            <Input
+                                labelText="Create new template"
+                                name="title"
+                                type="text"
+                                value={this.state.title}
+                                placeholder="e.g Intro to Web Development"
+                                handleChange={this.handleInput}
+                                labelInline
+                                largeLabel
+                            />
+                            <button className="success">Save Template</button>
+                        </div>
+                    </form>
+                </section>
             </section>
-        </div>
-    );
+        );
+    }
+}
+
+CourseTemplates.propTypes = {
+    actions: PropTypes.shape({
+        broadcast: PropTypes.func.isRequired,
+        createTemplateThunk: PropTypes.func.isRequired,
+    }).isRequired,
 };
 
 export default CourseTemplates;
--- src/components/CourseTemplates/CourseTemplatesContainer.js
+++ src/components/CourseTemplates/CourseTemplatesContainer.js
@@ -1,4 +1,19 @@
 import { connect } from 'react-redux';
-import CourseTemplates from './CourseTemplates';
+import { bindActionCreators } from 'redux';
 
-export default connect(null, null)(CourseTemplates);
+import CourseTemplates from './CourseTemplates';
+import * as broadcastActions from '../../actions/broadcastActions/broadcastActions';
+import * as courseActions from '../../actions/courseActions/courseActions';
+
+const mapStateToProps = (state) => {
+    const newState = Object.assign({}, state);
+    return newState;
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators({ ...courseActions, ...broadcastActions }, dispatch),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CourseTemplates);
