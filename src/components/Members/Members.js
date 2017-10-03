3241419627c5486fb78275cd3d0a6afdca125b38
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2017-10-03 18:03:48 -0400

    added ui components to Members page

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -1,8 +1,63 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Members = () => {
     return (
-        <div>Woop Woop! hello Linting!</div>
+        <section className="mainContent">
+            <div className="container">
+                <header className="topContent">
+                    <Link className="linkBtn button primary" to="/dashboard">
+                        back to dashboard
+                    </Link>
+                </header>
+            </div>
+            <section className="full card detailsForm">
+                <form className="addMembersForm" action="">
+                    <div className="fieldRow">
+                        <label
+                            className="inline largeLabel"
+                            htmlFor="add"
+                        >Add by email
+                        </label>
+                        <div className="inlineRow">
+                            <input id="add" type="text" />
+                        </div>
+                        <button className="success">Add User</button>
+                    </div>
+                </form>
+                <form action="">
+                    <div className="fieldRow">
+                        <label
+                            className="inline largeLabel"
+                            htmlFor="search"
+                        >Search by name:
+                        </label>
+                        <input type="text" id="search" />
+                        <button className="primary">Search</button>
+                    </div>
+                </form>
+            </section>
+            <div className="container card memberWrap">
+                <ul className="memberList">
+                    <li>
+                        <p>
+                            <strong>Sylvia Nguyen</strong>
+                        </p>
+                        <p>sylvia@hackeryou.com</p>
+                        <p>
+                            <span>1</span>
+                            <span>Classroom</span>
+                        </p>
+                        <div className="inputBlock">
+                            <div className="fieldRow">
+                                <label htmlFor="12">Instructor</label>
+                                <input type="checkbox" id="12" />
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
     );
 };
 
