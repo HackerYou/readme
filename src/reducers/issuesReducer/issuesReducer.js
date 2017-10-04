7835710d92176933edd24e58901e2e473ad58f38
Kristen Spencer <kristen@kristenspencer.ca>
2017-10-04 17:20:45 -0400

    renamed method

--- src/reducers/issuesReducer/issuesReducer.js
+++ src/reducers/issuesReducer/issuesReducer.js
@@ -1,13 +1,13 @@
 import types from '../../actions/actionTypes';
 import initialState from '../initialState';
 
-import stringify from '../../utils/stringify';
+import objClone from '../../utils/objClone';
 
 export default function (state = initialState.issues, action) {
     switch (action.type) {
 
     case types.UPDATE_ISSUES: {
-        const newState = stringify(state);
+        const newState = objClone(state);
         Object.assign(newState, { issues: action.issues });
         return newState;
     }
--- src/utils/stringify.js
+++
@@ -1,5 +0,0 @@
-const stringify = (data) => {
-    return JSON.parse(JSON.stringify(data));
-};
-
-export default stringify;
