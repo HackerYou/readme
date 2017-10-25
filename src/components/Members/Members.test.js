2c9a95a9f9d77483faf01d31484fafb1a900a220
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2017-10-25 17:37:10 -0400

    Removes mock function and replaces it with empty function for better readability

--- src/components/Members/Members.test.js
+++ src/components/Members/Members.test.js
@@ -24,7 +24,7 @@ describe('spy addUser()', () => {
       const test = shallow(<Members {...props} />)
 
       const wrapper = test.find('.createUser');
-      wrapper.simulate('submit', { preventDefault: jest.fn() });
+      wrapper.simulate('submit', { preventDefault:function(){}});
       expect(spy).toHaveBeenCalled();
     });
 });
