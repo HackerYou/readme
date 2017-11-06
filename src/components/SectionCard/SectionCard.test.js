2c3786694d47712b3d3c4370d342c658b5a5b038
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2017-11-06 14:49:05 -0500

    Adds tests to test to make sure user can add and remove sections

---
+++ src/components/SectionCard/SectionCard.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SectionCard from './SectionCard';
+import renderer from 'react-test-renderer';
+
+test('It calls the function that gets passed in as a prop to remove section on click.', () => {
+  const mockOnClick = jest.fn();
+
+  const props = {
+    title: 'This is a Section Title',
+    id: '98748345934',
+    lessons: [{}],
+  }
+
+  const sectionCard = shallow(<SectionCard {...props} removeSection={mockOnClick} />);
+  sectionCard.find('.deleteSection').simulate('click', { target: { value: 'Removes section' } }); 
+});
